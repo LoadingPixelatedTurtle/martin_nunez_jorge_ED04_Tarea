3330fedf3686fbaa8e8481e548b7a92c5c705554
LoadingPixelatedTurtle <loadingpixelatedturtle@gmail.com>
2022-02-13 12:01:56 +0100

    Punto 3 y Punto 4
    Encapsula los cuatro atributos de la clase CCuenta.
    Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad y de tipo float

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -6,43 +6,41 @@ package cuentas;
 
 public class CCuenta {
 
-
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
 
-    public CCuenta()
-    {
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
+
     /**
      * @return the nombre
      */
@@ -99,4 +97,3 @@ public class CCuenta {
         this.tipoInteres = tipoInteres;
     }
 }
-
--- src/cuentas/main.java
+++ src/cuentas/main.java
@@ -12,6 +12,7 @@ public class main {
     private static void operativa_cuenta() {
         CCuenta cuenta1;
         double saldoActual;
+        float cantidad;
         cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
